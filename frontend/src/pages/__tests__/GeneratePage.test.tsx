f64a179b556d2c6b4eabe8fb6fc0a7dfcb796080
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-10-09 21:24:47 +0530

    remove un-necessary import in test file

--- frontend/src/pages/__tests__/GeneratePage.test.tsx
+++ frontend/src/pages/__tests__/GeneratePage.test.tsx
@@ -1,5 +1,3 @@
-import "@testing-library/jest-dom";
-
 import GeneratePage from "../GeneratePage";
 import { render, waitFor } from "@testing-library/react";
 import { screen } from "@testing-library/dom";
